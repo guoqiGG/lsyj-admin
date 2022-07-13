332fcdf5233c985318708f9e0be978a102fd6835
dingtalk_haawzc_vxR <2275182981@qq.com>
2022-07-13 23:36:11 +0800

    测试

--- src/mock/index.js
+++ src/mock/index.js
@@ -10,6 +10,8 @@ const produceNewsData = function () {
     let newsList = []
     for (let i = 0; i < 20; i++) {
         let newNewsObject = {
+
+
             title: Random.ctitle(), //  Random.ctitle( min, max ) 随机产生一个中文标题，长度默认在3-7之间
             content: Random.cparagraph(), // Random.cparagraph(min, max) 随机生成一个中文段落，段落里的句子个数默认3-7个
             createdTime: Random.date() // Random.date()指示生成的日期字符串的格式,默认为yyyy-MM-dd；
