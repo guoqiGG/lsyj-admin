acbe408b860673d719e5f3b042a9ae568cd9a417
Zhang_yu <2275182981@qq.com>
2022-07-21 21:24:24 +0800

    加油

--- src/router/router.js
+++ src/router/router.js
@@ -1,53 +1,27 @@
-import {
-  createRouter,
-  createWebHashHistory,
-} from 'vue-router'
-import Home from '../views/home/index.vue'
-import login from '../views/login/index.vue'
-import Layout from '../layout/index.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "../views/home/index.vue";
+import login from "../views/login/index.vue";
+import Layout from "../layout/index.vue";
+import store from "../store";
 
-
-
-const routes = [{
-    path: '/login',
+const routes = [
+  {
+    path: "/login",
     name: login,
-    component: login
+    component: login,
   },
   {
-    path: '/',
-    name: 'home',
-    component: Layout
-  }
-]
-export const dynamicRouters = [{
-    path: '/',
+    path: "/",
+    name: "home",
     component: Layout,
-    name: 'container',
-    redirect: 'home',
-    meta: {
-      requiresAuth: true, //有一些页面是否登录才能进去  
-      name: '首页'
-    },
-    children: [{
-      path: 'home',
-      component: () => import('../views/home/index.vue'),
-      name: 'home',
-      mate: {
-        name: '首页',
-        icon: 'icon-name'
-      }
-    }]
   },
+];
+export const dynamicRouters = store.getters.menuList;
+// routes.push(dynamicRouters);
 
-  {
-    path: "/404",
-    name: "404",
-    component: () => import("../views/ErrorMessage/404.vue"),
-  },
-]
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
