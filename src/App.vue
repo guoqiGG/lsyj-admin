de2a1d224b22c0624e3a20fa3de310c43d7967fc
dingtalk_haawzc_vxR <2275182981@qq.com>
2022-08-06 00:25:15 +0800

    更换主题色

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,14 @@
   <!-- </el-config-provider> -->
 </template>
 
-<script  setup>
+<script setup>
+import { onMounted } from 'vue'
 
-
+onMounted(() => {
+  document.body.style.setProperty('--el-color-primary', '#4060c7')
+  document.body.style.setProperty('--el-color-primary-light-9', '#F5FBF0')
+  document.body.style.setProperty('--el-color-primary-light-3', '#6c88d5')
+})
 </script>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
