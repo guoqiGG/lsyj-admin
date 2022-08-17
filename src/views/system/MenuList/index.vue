dc96ec52750b5394d1b69491ad27ad1e0d7e7c29
dingtalk_haawzc_vxR <2275182981@qq.com>
2022-08-17 22:36:11 +0800

    编辑表格

---
+++ src/views/system/MenuList/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <el-card>
+    <el-button type="primary" :icon="Search" class="ml20">编辑表格</el-button>
+  </el-card>
+
+  <el-card class="mt10">
+    <el-table
+      :data="tableData"
+      :header-cell-style="{ backgroundColor: '#ecf5ff' }"
+      border
+      stripe
+      style="width: 100%"
+    >
+      <el-table-column
+        v-for="item in options"
+        :key="item.type"
+        :prop="item.props"
+        :label="item.label"
+        :width="item.width"
+        :align="item.align"
+        show-overflow-tooltip
+        :fixed="item.fixed"
+      >
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      class="mt10"
+      v-model:currentPage="currentPage4"
+      v-model:page-size="pageSize4"
+      :page-sizes="[100, 200, 300, 400]"
+      :small="small"
+      :disabled="disabled"
+      background
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </el-card>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+const tableData = ref([])
+
+const options = reactive([
+  {
+    label: '用户名',
+    props: 'username',
+    width: 'auto',
+    align: 'left',
+  },
+  {
+    label: '邮箱',
+    props: 'email',
+    width: 'auto',
+    align: 'left',
+  },
+  {
+    label: '创建时间',
+    props: 'date',
+    width: 'auto',
+    align: 'left',
+  },
+  {
+    label: '地址',
+    props: 'address',
+    width: 'auto',
+    align: 'left',
+  },
+  {
+    label: '备注',
+    props: 'content',
+    width: 'auto',
+    align: 'left',
+    show: true,
+  },
+  {
+    label: '操作',
+    props: 'actions',
+    width: '150',
+    align: 'center',
+    fixed: 'right',
+  },
+])
+</script>
