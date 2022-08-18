af9626804931e95856abcd8069027546bb18ad51
Zhang_yu <2275182981@qq.com>
2022-08-18 19:06:14 +0800

    可编辑表格

--- src/views/system/MenuList/index.vue
+++ src/views/system/MenuList/index.vue
@@ -1,125 +1,129 @@
 <template>
   <el-card>
-    <el-button type="primary" :icon="Search" class="ml20">编辑表格</el-button>
+    <el-button type="primary" class="mt-4" @click="dialogVisible = true">编辑表格</el-button>
   </el-card>
-
   <el-card class="mt10">
-    <el-table
-      :data="tableData"
-      :header-cell-style="{ backgroundColor: '#ecf5ff' }"
-      border
-      stripe
-      style="width: 100%"
-    >
+    <el-table :data="tableData" :header-cell-style="{ backgroundColor: '#ecf5ff' }" border stripe style="width: 100%">
       <el-table-column
-        v-for="item in options"
-        :key="item.type"
+        v-for="item in columnData"
+        :key="item.props"
         :prop="item.props"
         :label="item.label"
         :width="item.width"
         :align="item.align"
         show-overflow-tooltip
         :fixed="item.fixed"
-      >
-      </el-table-column>
+      ></el-table-column>
     </el-table>
     <el-pagination
-      class="mt10"
-      v-model:currentPage="currentPage4"
-      v-model:page-size="pageSize4"
-      :page-sizes="[100, 200, 300, 400]"
+      class="mt10 mt-4"
+      v-model:currentPage="queryData.page"
+      v-model:page-size="queryData.size"
+      :page-sizes="[10]"
       :small="small"
       :disabled="disabled"
       background
       layout="total, sizes, prev, pager, next, jumper"
-      :total="400"
+      :total="total"
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
     />
   </el-card>
 
-  <el-dialog
-    v-model="dialogVisible"
-    :title="`${dialogData.title}用户`"
-    :width="dialogWidth"
-    :hide-required-asterisk="dialogData.isView"
-    draggable
-  >
-    <el-form
-      :model="dialogData.FormData"
-      :rules="rules"
-      ref="FormRef"
-      class="demo-ruleForm"
-    >
-      <el-form-item
-        label="用户名"
-        :label-width="formLabelWidth"
-        prop="username"
-      >
-        <el-input v-model="dialogData.FormData.username" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="邮箱" :label-width="formLabelWidth" prop="email">
-        <el-input v-model="dialogData.FormData.email" autocomplete="off" />
-      </el-form-item>
-
-      <el-form-item label="地址" :label-width="formLabelWidth" prop="address">
-        <el-input v-model="dialogData.FormData.address" type="textarea" />
-      </el-form-item>
-      <el-form-item label="备注" :label-width="formLabelWidth" prop="content">
-        <el-input v-model="dialogData.FormData.content" type="textarea" />
-      </el-form-item>
-    </el-form>
+  <el-dialog v-model="dialogVisible" title="编辑列表" width="80%" :before-close="handleClose">
+    <el-table :data="tableDialog" :header-cell-style="{ backgroundColor: '#ecf5ff' }" border stripe style="width: 100%">
+      <el-table-column
+        v-for="item in DialogColumn"
+        :key="item.props"
+        :prop="item.props"
+        :label="item.label"
+        :width="item.width"
+        :align="item.align"
+        show-overflow-tooltip
+        :fixed="item.fixed"
+      ></el-table-column>
+    </el-table>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="onSubmit()">确认</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确认</el-button>
       </span>
     </template>
   </el-dialog>
 </template>
-
 <script setup>
-import { reactive, ref } from 'vue'
-const tableData = ref([])
+import { onMounted, reactive, ref } from "vue";
+import { getUserList } from "../../../api/modules/index.js";
+import { options } from "./options.js";
+const tableData = ref([]);
+const tableDialog = ref([]);
+const DialogColumn = ref([
+  {
+    label: "表头",
+    props: "username",
+    width: "auto",
+    align: "left",
+  },
+  {
+    label: "对应列的宽度",
+    props: "email",
+    width: "auto",
+    align: "left",
+  },
+  {
+    label: "对齐方式",
+    props: "date",
+    width: "auto",
+    align: "left",
+  },
+  {
+    label: "是否显示",
+    props: "date",
+    width: "auto",
+    align: "left",
+  },
+  {
+    label: "列是否固定",
+    props: "date",
+    width: "auto",
+    align: "left",
+  },
+]);
 
-const options = reactive([
-  {
-    label: '用户名',
-    props: 'username',
-    width: 'auto',
-    align: 'left',
-  },
-  {
-    label: '邮箱',
-    props: 'email',
-    width: 'auto',
-    align: 'left',
-  },
-  {
-    label: '创建时间',
-    props: 'date',
-    width: 'auto',
-    align: 'left',
-  },
-  {
-    label: '地址',
-    props: 'address',
-    width: 'auto',
-    align: 'left',
-  },
-  {
-    label: '备注',
-    props: 'content',
-    width: 'auto',
-    align: 'left',
-    show: true,
-  },
-  {
-    label: '操作',
-    props: 'actions',
-    width: '150',
-    align: 'center',
-    fixed: 'right',
-  },
-])
+const columnData = ref(JSON.parse(JSON.stringify(options)));
+const disabled = ref(false);
+const dialogVisible = ref(true);
+const total = ref("");
+const queryData = ref({
+  keyWord: "",
+  page: 1,
+  size: 10,
+});
+const initData = () => {
+  getUserList(queryData.value).then((res) => {
+    total.value = res.data.data.total;
+    tableData.value = res.data.data.userList;
+  });
+};
+const handleSizeChange = (val) => {
+  console.log(val);
+};
+
+const handleCurrentChange = (val) => {
+  queryData.value.page = val;
+  initData(queryData.value);
+};
+
+onMounted(() => {
+  console.log(columnData.value);
+  initData();
+});
 </script>
+
+<style lang="scss" scoped>
+.mt-4 {
+  float: right;
+  text-align: right;
+  margin-bottom: 20px;
+}
+</style>
