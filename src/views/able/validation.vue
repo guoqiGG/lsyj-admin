ea50e80272b5d923074bbc8946dd47300c8770fd
Zhang_yu <2275182981@qq.com>
2022-08-22 18:24:32 +0800

    验证码组件

--- src/views/able/validation.vue
+++ src/views/able/validation.vue
@@ -1,135 +1,188 @@
 <template>
   <el-card class="clo-center">
-    <el-form
-      label-position="left"
-      label-width="100px"
-      :model="formLabelAlign"
-      style="width: 460px"
-    >
-      <el-form-item label="请输入验证码">
+    <el-form label-position="left" inline label-width="100px" :model="formLabelAlign">
+      <el-form-item label="请输入验证码" style="margin-right: 0">
         <el-input v-model="formLabelAlign.code" />
-        <div class="s-canvas">
-          <canvas
-            @click="updataCode"
-            id="codeCanves"
-            :width="contentWidth"
-            :height="contentHeight"
-          ></canvas>
-        </div>
       </el-form-item>
+      <el-form-item>
+        <div class="s-canvas">
+          <canvas @click="upDataCode" ref="canvasRef" :width="contentWidth" :height="contentHeight"></canvas></div
+      ></el-form-item>
     </el-form>
+    <el-button type="primary">拼图验证</el-button>
+    <div className="sliderContent">
+      <div className="imgDev" style="width: 500px">
+        <canvas id="canvasImg" width="500" height="auto"></canvas>
+        <canvas className="slider" id="sliderBlock" :width="sliderWidth" :height="sliderHeight" :style="{ left: sildLeft }"></canvas>
+      </div>
+    </div>
   </el-card>
 </template>
 
 <script setup>
-import { onMounted, reactive, ref } from 'vue'
-const contentWidth = ref(112)
-const contentHeight = ref(40)
+import { width } from "dom7";
+import { onMounted, reactive, ref } from "vue";
+const contentWidth = ref(100);
+const contentHeight = ref(32);
+const sliderWidth = ref(32);
+const sliderHeight = ref(32);
+const sildLeft = ref(0);
+const sildY = ref();
 const formLabelAlign = reactive({
-  code: '',
-})
-const identifyCodes = ref('3456789ABCDEFGHGKMNPQRSTUVWXY')
-const identifyCode = ref('1234')
-const backgroundColorMin = ref(180)
-const backgroundColorMax = ref(240)
-const colorMin = ref(50)
-const colorMax = ref(160)
-const lineColorMin = ref(30)
-const lineColorMax = ref(120)
-const dotColorMin = ref(0)
-const dotColorMax = ref(255)
+  code: "",
+});
+const identifyCodes = ref("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+const identifyCode = ref("1234");
+const canvasRef = ref(null);
 
+// 生成随机数
 const randomNum = (min, max) => {
-  return Math.floor(Math.random() * (max - min) + min)
-}
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
 const randomColor = (min, max) => {
-  var r = randomNum(min, max)
-  var g = randomNum(min, max)
-  var b = randomNum(min, max)
-  return 'rgb(' + r + ',' + g + ',' + b + ')'
-}
+  var r = randomNum(min, max);
+  var g = randomNum(min, max);
+  var b = randomNum(min, max);
+  return "rgb(" + r + "," + g + "," + b + ")";
+};
 
 const drawPic = () => {
-  var canvas = document.getElementById('codeCanves')
-  var ctx = canvas.getContext('2d')
-  ctx.textBaseline = 'bottom' // 绘制背景
-  ctx.fillStyle = randomColor(
-    backgroundColorMin.value,
-    backgroundColorMax.value
-  )
-  ctx.fillRect(0, 0, contentWidth.value, contentHeight.value) // 绘制文字
-  // for (let i = 0; i < identifyCode.value.length; i++) {
-  drawText(ctx, identifyCode.value)
-  // }
-  drawLine(ctx)
-  drawDot(ctx)
-}
-const drawText = (ctx, txt, i) => {
-  console.log(txt)
-  ctx.fillStyle = randomColor(colorMin.value, colorMax.value)
-  ctx.font = randomNum(28, 40) + 'px SimHei'
-  var x = contentWidth.value / (identifyCode.length + 1)
-  var y = randomNum(40, contentHeight.value - 5)
-  var deg = randomNum(-30, 30) // 修改坐标原点和旋转角度
-  ctx.translate(x, y)
-  ctx.rotate((deg * Math.PI) / 270)
-  ctx.fillText(txt, 20, 30) // 恢复坐标原点和旋转角度
-  console.log(txt)
-  ctx.rotate((-deg * Math.PI) / 270)
-  ctx.translate(-x, -y)
-}
+  let ctx = canvasRef.value.getContext("2d");
+  ctx.fillStyle = randomColor(180, 230); //填充颜色
+  ctx.fillRect(0, 0, contentWidth.value, contentHeight.value); //填充位置
+  // drawLine(ctx);
+
+  let imgCode = "";
+  // 随机产生字符串，并且随机旋转
+  for (let i = 0; i < 4; i++) {
+    const text = identifyCodes.value[randomNum(0, identifyCodes.value.length)];
+    const deg = randomNum(-30, 30);
+    imgCode += text;
+    ctx.font = randomNum(18, 40) + "px Simhei"; //填充字体
+    ctx.textBaseline = "top";
+    ctx.fillStyle = randomColor(80, 150);
+    drawDot(ctx);
+    ctx.save(); //保存
+    ctx.translate(30 * i + 15, 15);
+    ctx.rotate((deg * Math.PI) / 180); //随机的旋转
+    ctx.fillText(text, -15 + 5, -15); //
+    ctx.restore(); //清除
+  }
+  drawLine(ctx);
+  return imgCode;
+};
+
 const drawLine = (ctx) => {
-  // 绘制干扰线
-  for (let i = 0; i < 2; i++) {
-    ctx.strokeStyle = randomColor(lineColorMin.value, lineColorMax.value)
-    ctx.beginPath()
-    ctx.moveTo(
-      randomNum(0, contentWidth.value),
-      randomNum(0, contentHeight.value)
-    )
-    ctx.lineTo(
-      randomNum(0, contentWidth.value),
-      randomNum(0, contentHeight.value)
-    )
-    ctx.stroke()
+  for (let i = 0; i < 5; i++) {
+    ctx.strokeStyle = randomColor(0, 255);
+    ctx.beginPath();
+    ctx.moveTo(randomNum(0, contentWidth.value), randomNum(0, contentHeight.value));
+    ctx.lineTo(randomNum(0, contentWidth.value), randomNum(0, contentHeight.value));
+    ctx.stroke();
   }
-}
+};
 const drawDot = (ctx) => {
-  // 绘制干扰点
   for (let i = 0; i < 20; i++) {
-    ctx.fillStyle = randomColor(0, 255)
-    ctx.beginPath()
-    ctx.arc(
-      randomNum(0, contentWidth.value),
-      randomNum(0, contentHeight.value),
-      1,
-      0,
-      2 * Math.PI
-    )
-    ctx.fill()
+    ctx.fillStyle = randomColor(0, 255);
+    ctx.beginPath();
+    ctx.arc(randomNum(0, contentWidth.value), randomNum(0, contentHeight.value), 1, 0, 2 * Math.PI);
+    ctx.fill();
   }
-}
+};
 
 onMounted(() => {
-  drawPic()
-})
+  drawPic();
+  loadImage();
+});
 
-const updataCode = () => {
-  identifyCode.value = ''
-  makeCode(identifyCodes.value, 4)
-}
+const upDataCode = () => {
+  identifyCode.value = "";
+  makeCode(identifyCodes.value, 4);
+};
 const makeCode = (val, l) => {
   for (let i = 1; i < val.length && i <= l; i++) {
-    identifyCode.value +=
-      identifyCodes.value[
-        Math.floor(Math.random() * (identifyCodes.value.length - 0) + 0)
-      ]
+    identifyCode.value += identifyCodes.value[Math.floor(Math.random() * (identifyCodes.value.length - 0) + 0)];
   }
+  console.log(identifyCode.value);
+  drawPic();
+};
 
-  console.log(identifyCode.value)
-  drawPic()
-}
+// 拼图
+const loadImage = () => {
+  //加载图片
+  let mainDom = document.getElementById("canvasImg");
+  let bg = mainDom.getContext("2d");
+  let imgSrc =
+    "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F202001%2F30%2F20200130003214_3GJWF.jpeg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1663755123&t=0f35d18b29de642d5af549a5ec7062e5";
+  let img = document.createElement("img");
+  img.src = imgSrc;
+  img.onload = () => {
+    mainDom.height = (img.height / img.width) * mainDom.width;
+    bg.drawImage(img, 0, 0, mainDom.width, mainDom.height);
+    let r = 10; //半圆的半径
+    let w = 60; //滑块的宽度
+    let x = randomNum(85, mainDom.width - w - r - 2); //保证缺口完全展示，不会隐藏
+    let y = randomNum(16, mainDom.height - w - r - 2);
+    sildY.value = y;
+    bg.lineWidth = 1;
+    bg.strokeStyle = "white";
+    bg.beginPath();
+    bg.moveTo(x, y);
+    //top
+    bg.arc(x + w / 2, y, r, -Math.PI, 0, true);
+    bg.lineTo(x + w, y);
+    //right
+    bg.arc(x + w, y + w / 2, r, 1.5 * Math.PI, 0.5 * Math.PI, false);
+    bg.lineTo(x + w, y + w);
+    //bottom
+    bg.arc(x + w / 2, y + w, r, 0, Math.PI, false);
+    bg.lineTo(x, y + w);
+    //left
+    bg.arc(x, y + w / 2, r, 0.5 * Math.PI, 1.5 * Math.PI, true);
+    bg.lineTo(x, y);
+    bg.stroke();
+    bg.fillStyle = "rgba(0, 0, 0, 0.4)"; //设置背景颜色
+    bg.fill();
+
+    sliderContent();
+  };
+};
+
+const sliderContent = () => {
+  let r = 10; //半圆的半径
+  let w = 60; //滑块的宽度
+  let mainDom = document.getElementById("sliderBlock");
+  let ctx = mainDom.getContext("2d");
+  //index.store.ts
+  let x = sildLeft.value;
+  let y = sildY.value;
+  let PI = Math.PI;
+  ctx.lineWidth = 1;
+  //绘制
+  ctx.beginPath();
+  //left
+  ctx.moveTo(x, y);
+  //top
+  ctx.arc(x + w / 2, y, r, -PI, 0, true);
+  ctx.lineTo(x + w, y);
+  //right
+  ctx.arc(x + w, y + w / 2, r, 1.5 * PI, 0.5 * PI, false);
+  ctx.lineTo(x + w, y + w);
+  //bottom
+  ctx.arc(x + w / 2, y + w, r, 0, PI, false);
+  ctx.lineTo(x, y + w);
+  //left
+  ctx.arc(x, y + w / 2, r, 0.5 * PI, 1.5 * PI, true);
+  ctx.lineTo(x, y);
+  ctx.shadowBlur = 10;
+  ctx.shadowColor = "black";
+  ctx.stroke();
+  ctx.clip();
+  // ctx.drawImage(img, -this.x + this.sliderLeft, 0, width, height);
+};
+
+// https://www.jianshu.com/p/7feaf0aaacbf
 </script>
 
 <style lang="scss" scoped>
