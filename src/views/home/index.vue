356ff2a2ecd24fddbb028c4e0a3d8c44e8404d52
dingtalk_haawzc_vxR <2275182981@qq.com>
2022-08-31 23:52:06 +0800

    首页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,3 +1,78 @@
 <template>
-  <img src="../../assets/home.png" alt="" />
+  <el-row class="data-lists">
+    <el-col :span="9"
+      ><div class="data-item-one flx-row">
+        <div class="item-left">
+          <img src="../../assets/home.png" alt="" srcset="" />
+        </div>
+        <div class="item-right">
+          <div class="item-right-top">
+            <div class="tit">本期活动GMX</div>
+            <div class="num">1523.53w</div>
+          </div>
+          <div class="line"></div>
+          <div class="item-right-bottom flx-row">
+            <div class="item">
+              <div class="tit">类目一</div>
+              <div class="num">53w</div>
+            </div>
+            <div class="item">
+              <div class="tit">类目一</div>
+              <div class="num">53w</div>
+            </div>
+            <div class="item">
+              <div class="tit">类目一</div>
+              <div class="num">53w</div>
+            </div>
+          </div>
+        </div>
+      </div></el-col
+    >
+    <el-col :span="9"
+      ><div class="data-item-two">
+        <div class="item">
+          <div class="item-des flx-row">
+            <img src="../../assets/icon1.png" alt="" />
+            <div class="right">
+              <div class="num">934.7w <span>29.74</span></div>
+              <div class="txt">今日DAU</div>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="item-des flx-row">
+            <img src="../../assets/icon2.png" alt="" />
+            <div class="right">
+              <div class="num">264.7w <span>29.74</span></div>
+              <div class="txt">今日DAU</div>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="item-des flx-row">
+            <img src="../../assets/icon1.png" alt="" />
+            <div class="right">
+              <div class="num">934.7w <span>29.74</span></div>
+              <div class="txt">今日DAU</div>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="item-des flx-row">
+            <img src="../../assets/icon2.png" alt="" />
+            <div class="right">
+              <div class="num">934.7w <span>29.74</span></div>
+              <div class="txt">今日DAU</div>
+            </div>
+          </div>
+        </div>
+      </div></el-col
+    >
+    <el-col :span="6">数据三</el-col>
+    <!-- <el-col :span="6"><div class="data-item">数据一</div></el-col> -->
+  </el-row>
 </template>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
--- src/views/material/materialIndex.vue
+++ src/views/material/materialIndex.vue
@@ -1,21 +1,29 @@
 <template>
   <el-row style="height: 90vh">
-    <el-col :span="6" style="border-right: 10px solid #f0f2f5"> </el-col>
-    <el-col :span="18"></el-col>
+    <el-col :span="6" style="border-right: 10px solid #f0f2f5">
+      <div class="material-header flx-row">
+        <div class="tit">目录</div>
+        <div>
+          <el-icon><MoreFilled /></el-icon>
+        </div>
+      </div>
+    </el-col>
+    <el-col :span="18">
+      <div class="material-header flx-row">
+        <div class="tit">我的文件</div>
+        <div>
+          <el-icon><MoreFilled /></el-icon>
+        </div>
+      </div>
+      <div class="material-content">
+        <div class="flx-row">
+          <div class="item"></div>
+        </div>
+      </div>
+    </el-col>
   </el-row>
 </template>
 
-<style>
-.el-row {
-  margin-bottom: 20px;
-}
-.el-row:last-child {
-  margin-bottom: 0;
-}
-.el-col {
-  border-radius: 4px;
-  background-color: #fff;
-  /* margin-right: 20px; */
-  height: 100%;
-}
+<style lang="scss" scoped>
+@import './index.scss';
 </style>
