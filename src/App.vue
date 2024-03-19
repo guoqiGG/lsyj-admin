6066a9f3f09aac792e981a0c5841c55eaab70855
guoqiGG <2411074379@qq.com>
2024-03-19 14:11:55 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -24,3 +24,157 @@ onMounted(() => {
 </script>
 
 <style scoped lang="scss"></style>
+
+<style lang="scss">
+.el-button {
+  color: #78797d;
+  background-color: #FFF;
+  border-color: #dee1e7;
+
+  &:hover {
+    color: #78797d;
+    background-color: #FFF;
+    border-color: #dee1e7;
+  }
+
+  &:focus {
+    color: #78797d;
+    background-color: #FFF;
+    border-color: #dee1e7;
+  }
+
+  &:active {
+    color: #78797d;
+    background-color: #FFF;
+    border-color: #dee1e7;
+  }
+}
+
+
+.el-button--primary {
+  color: #fff;
+  background-color: #4060c7;
+  border-color: #4060c7;
+  font-size: 14px;
+
+  &:hover {
+    color: #FFF;
+    background-color: #4060c7;
+    border-color: #4060c7;
+  }
+
+  &:focus {
+    color: #FFF;
+    background-color: #4060c7;
+    border-color: #4060c7;
+  }
+
+  &:active {
+    color: #FFF;
+    background-color: #4060c7;
+    border-color: #4060c7;
+  }
+}
+
+.el-button--info {
+  color: #fff;
+  background-color: #909399;
+  border-color: #909399;
+  font-size: 14px;
+
+  &:hover {
+    color: #FFF;
+    background-color: #909399;
+    border-color: #909399;
+  }
+
+  &:focus {
+    color: #FFF;
+    background-color: #909399;
+    border-color: #909399;
+  }
+
+  &:active {
+    color: #FFF;
+    background-color: #909399;
+    border-color: #909399;
+  }
+}
+
+.el-button--success {
+  color: #fff;
+  background-color: #67c23a;
+  border-color: #67c23a;
+  font-size: 14px;
+
+  &:hover {
+    color: #FFF;
+    background-color: #67c23a;
+    border-color: #67c23a;
+  }
+
+  &:focus {
+    color: #FFF;
+    background-color: #67c23a;
+    border-color: #67c23a;
+  }
+
+  &:active {
+    color: #FFF;
+    background-color: #67c23a;
+    border-color: #67c23a;
+  }
+}
+
+.el-button--warning {
+  color: #fff;
+  background-color: #e6a23c;
+  border-color: #e6a23c;
+  font-size: 14px;
+
+  &:hover {
+    color: #FFF;
+    background-color: #e6a23c;
+    border-color: #e6a23c;
+  }
+
+  &:focus {
+    color: #FFF;
+    background-color: #e6a23c;
+    border-color: #e6a23c;
+  }
+
+  &:active {
+    color: #FFF;
+    background-color: #e6a23c;
+    border-color: #e6a23c;
+  }
+}
+
+.el-button--danger {
+  color: #fff;
+  background-color: #f56c6c;
+  border-color: #f56c6c;
+  font-size: 14px;
+
+  &:hover {
+    color: #FFF;
+    background-color: #f56c6c;
+    border-color: #f56c6c;
+  }
+
+  &:focus {
+    color: #FFF;
+    background-color: #f56c6c;
+    border-color: #f56c6c;
+  }
+
+  &:active {
+    color: #FFF;
+    background-color: #f56c6c;
+    border-color: #f56c6c;
+  }
+}
+
+
+</style>
